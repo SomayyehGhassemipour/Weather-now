0ffb685f6c3e2f1cc28e9b19f2d887673809676e
Somayyeh <Somayyeh.ghassemipour@gmail.com>
2022-08-19 16:05:59 +0200

    Added Search box

---
+++ src/Api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '7982dd216emsha03616eb599d809p1d06d6jsnc7239d32ca47',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
