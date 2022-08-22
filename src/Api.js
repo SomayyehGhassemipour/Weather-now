8b885ff3427cd3e1870ec2c5635002cb4a357206
Somayyeh <Somayyeh.ghassemipour@gmail.com>
2022-08-22 16:47:34 +0200

    added current weather box

--- src/Api.js
+++ src/Api.js
@@ -8,3 +8,6 @@ export const geoApiOptions = {
 
 
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "ec8a8eacc6b82f25a1d7ba9a1142c6f8";
--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
 import './App.css';
+import CurrentWeatherBox from './components/current weather/CurrentWeatherBox';
 import Search from './components/search box/Search';
+import { WEATHER_API_URL, WEATHER_API_KEY } from './Api';
+import { useState } from 'react';
 
 function App() {
+
+  const [currentWeather, seCurrentWeather] = useState(null);
+  const [Forecast, setForecast] = useState(null);
+
   const HandleonSearchChange = (searchData) => {
     console.log(searchData);
+    const [lat, lon] = searchData.value.split(" ");
+
+    const currentWeatherFetch = fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`)
+    const forecastFetch = fetch(`${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`)
+
+    Promise.all([currentWeatherFetch, forecastFetch])
+      .then(async (response) => {
+        const weatherResponse = await response[0].json();
+        const forecastResponse = await response[1].json();
+        seCurrentWeather({ city: searchData.label, ...weatherResponse });
+        setForecast({ city: searchData.label, ...forecastResponse });
+      })
+      .catch((error) => console.log(error))
   }
-  
+
+  console.log(currentWeather);
+  console.log(Forecast);
+
   return (
     <div className="container">
-    <Search onSearchChange={HandleonSearchChange}/>
+      <Search onSearchChange={HandleonSearchChange} />
+      {currentWeather && <CurrentWeatherBox data={currentWeather} />}
     </div>
   );
 }
